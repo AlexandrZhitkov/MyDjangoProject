b529b75bc9ea74562df3c8e6262eb139a13d5956
AlexGagarin93 <smackdownmaps1317@yandex.ru>
2021-11-04 17:11:46 +0300

    added filtering by category

--- products/views.py
+++ products/views.py
@@ -10,13 +10,18 @@ def index(request):
     }
     return render(request, 'products/index.html', context)
 
-def products(request):
+def products(request, category_id=None):
     db_categories = ProductCategory.objects.all()
     db_products = Product.objects.all()
     context = {'title': 'GeekShop - Каталог',
                 'products': db_products,
                 'categories': db_categories,
                }
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    context['products'] = products
 
     return render(request,'products/products.html', context)
 
