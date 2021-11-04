7011fcac855a70186789d5d1e7b0326722d6d337
AlexGagarin93 <smackdownmaps1317@yandex.ru>
2021-11-04 19:09:42 +0300

    added dispatch

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -17,6 +18,10 @@ def index(request):
 class UserListView(ListView):
     model = User
     template_name = 'admins/admin-users-read.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 class UserCreateView(CreateView):
     model = User
